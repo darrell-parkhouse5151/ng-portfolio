f3cae7a224b7f61b4057ebd50bf08a9e99a71e87
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-05-18 11:53:33 -0500

    client model

---
+++ src/app/models/client.ts
@@ -0,0 +1,10 @@
+/* tslint:disable*/
+export interface Client {
+    id?: string;
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    phoneNumber?: string;
+    balance?: number;
+
+}
