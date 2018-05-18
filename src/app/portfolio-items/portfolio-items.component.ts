1cd9ff11823d9b919a23a70c83492e7cedbfab18
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-05-18 11:58:53 -0500

    no logic necessary

---
+++ src/app/portfolio-items/portfolio-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-items',
+  templateUrl: './portfolio-items.component.html',
+  styleUrls: ['./portfolio-items.component.scss']
+})
+export class PortfolioItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
