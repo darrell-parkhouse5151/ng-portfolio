3849db9cd7aad3f2dc6b7c2f1220d9fc0e3d601e
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-05-18 12:09:25 -0500

    no need for logic

---
+++ src/app/projects/quantum/quantum-header/quantum-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quantum-header',
+  templateUrl: './quantum-header.component.html',
+  styleUrls: ['./quantum-header.component.scss']
+})
+export class QuantumHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
