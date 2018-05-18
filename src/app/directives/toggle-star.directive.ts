342dfb054b199116a8799cd230363399d54ad56b
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-05-18 11:52:47 -0500

    custom directive for toggling the star for the email component

---
+++ src/app/directives/toggle-star.directive.ts
@@ -0,0 +1,19 @@
+/* tslint:disable*/
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+    selector: '[appToggleStar]'
+})
+export class ToggleStarDirective {
+
+    constructor(private elem: ElementRef) {
+    }
+
+    @HostListener('click', ['$event'])
+    onToggle($event: any) {
+        $event.preventDefault();
+
+        this.elem.nativeElement.classList.toggle('star-is-selected');
+        this.elem.nativeElement.classList.toggle('star-not-selected');
+    }
+}
