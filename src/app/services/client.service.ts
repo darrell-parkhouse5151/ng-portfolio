926cc488771571c7f8b484b38b6ab95a3e1f61c8
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-05-19 08:46:27 -0500

    simple client service for the client panel

---
+++ src/app/services/client.service.ts
@@ -0,0 +1,67 @@
+/* tslint:disable*/
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import {Observable} from 'rxjs/Observable';
+
+import {Client} from '../models/client';
+
+@Injectable()
+export class ClientService {
+    clientCollection: AngularFirestoreCollection<Client>;
+    clientDoc: AngularFirestoreDocument<Client>;
+    clients: Observable<Client[]>;
+    client: Observable<Client>;
+
+    constructor(private angularFirestore: AngularFirestore) {
+        this.angularFirestore.firestore.settings({ timestampsInSnapshots: true });
+        this.clientCollection = this.angularFirestore.collection('clients', ref => ref.orderBy('lastName', 'asc'));
+    }
+
+    getClients() : Observable<Client[]> {
+        this.clients = this.clientCollection.snapshotChanges().map(change => {
+            return change.map(action => {
+                const data = action.payload.doc.data() as Client;
+                data.id = action.payload.doc.id;
+                return data;
+            });
+        });
+
+        return this.clients;
+    }
+
+    newClient(client: Client) {
+        this.clientCollection.add(client)
+            .then(() => console.log('success'))
+            .catch(err => console.log(err));
+    }
+
+    getClient(id: string) : Observable<Client> {
+        this.clientDoc = this.angularFirestore.doc<Client>(`clients/${id}`);
+
+        this.client = this.clientDoc.snapshotChanges().map(action => {
+            if (action.payload.exists === false) {
+                return null;
+            } else {
+                const data = action.payload.data() as Client;
+                data.id = action.payload.id;
+                return data;
+            }
+        });
+
+        return this.client;
+    }
+
+    updateClient(client: Client) {
+        this.clientDoc = this.angularFirestore.doc(`clients/${client.id}`);
+        this.clientDoc.update(client)
+            .then(() => console.log('successfully updated client'))
+            .catch(err => console.log(err));
+    }
+
+    deleteClient(client: Client) {
+        this.clientDoc = this.angularFirestore.doc(`clients/${client.id}`);
+        this.clientDoc.delete()
+            .then(() => console.log('successfully removed client'))
+            .catch(err => console.log(err));
+    }
+}
