42fc02790f18d3362aef235e707c9fc19900fd4c
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-05-19 08:43:06 -0500

    logic for the epic ecommerce component

---
+++ src/app/projects/epic/epic-ecommerce/epic-ecommerce.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-epic-ecommerce',
+  templateUrl: './epic-ecommerce.component.html',
+  styleUrls: ['./epic-ecommerce.component.scss']
+})
+export class EpicEcommerceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
