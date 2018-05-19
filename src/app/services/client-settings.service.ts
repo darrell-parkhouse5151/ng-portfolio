5768203295a833581430231a99975621a9c39fb5
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-05-19 08:48:53 -0500

    simple client-settings service for the client panel

---
+++ src/app/services/client-settings.service.ts
@@ -0,0 +1,27 @@
+/* tslint:disable*/
+import { Injectable } from '@angular/core';
+import { ClientSettings } from '../models/client-settings';
+
+@Injectable()
+export class ClientSettingsService {
+    settings: ClientSettings = {
+        allowRegistration: false,
+        disableBalanceOnAdd: true,
+        disableBalanceOnEdit: false
+    };
+
+    constructor() {
+        if (localStorage.getItem('settings') !== null) {
+            this.settings = JSON.parse(localStorage.get('settings'));
+        }
+    }
+
+    getSettings() : ClientSettings {
+        return this.settings;
+    }
+
+    changeSettings(settings: ClientSettings) {
+        localStorage.setItem('settings', JSON.stringify(settings));
+    }
+
+}
