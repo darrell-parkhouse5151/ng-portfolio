52b7af00e30ad6bf5131ca8790f459a8110aa5b7
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-05-19 08:34:18 -0500

    logic for the leads component

---
+++ src/app/projects/quantum/quantum-leads/quantum-leads.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quantum-leads',
+  templateUrl: './quantum-leads.component.html',
+  styleUrls: ['./quantum-leads.component.scss']
+})
+export class QuantumLeadsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
