54f6e0a4aeebf0237f6ce548534960623f9e789d
darrell-parkhouse5151 <lazycoder5151@gmail.com>
2018-05-21 12:51:08 -0500

    added functionality to add and remove the quantum class from the body

--- src/app/projects/quantum/quantum-email/compose-email/compose-email.component.ts
+++ src/app/projects/quantum/quantum-email/compose-email/compose-email.component.ts
@@ -1,12 +1,12 @@
 /* tslint:disable*/
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
     selector: 'compose-email',
     templateUrl: './compose-email.component.html',
     styleUrls: ['./compose-email.component.scss']
 })
-export class ComposeEmailComponent implements OnInit {
+export class ComposeEmailComponent implements OnInit, OnDestroy {
     pageName: string = 'Compose Email';
     componentName: string = 'Compose New Email';
     constructor() {
@@ -16,4 +16,7 @@ export class ComposeEmailComponent implements OnInit {
         document.body.classList.add('quantum');
     }
 
+    ngOnDestroy() {
+        document.body.classList.remove('quantum');
+    }
 }
